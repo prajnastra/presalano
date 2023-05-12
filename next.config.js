0db504efab6da19e470d20fbfcb6568236297009
prajnastra <coder.abhijit90@gmail.com>
2023-05-12 07:30:39 +0530

    add: prettier & pre commit hooks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BLOCKFOREST_KEY: process.env.BLOCKFOREST_KEY,
+    NFTSTORAGE_KEY: process.env.NFTSTORAGE_KEY,
+  },
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
