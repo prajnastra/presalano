1281180381e23470552acaf889c6e293c49699b3
prajnastra <coder.abhijit90@gmail.com>
2023-05-12 19:46:32 +0530

    add: react-hook-form

--- client/pages/minter/index.tsx
+++ client/pages/minter/index.tsx
@@ -1,31 +1,88 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+// import swal from 'sweetalert'
+
 import {
   Box,
   Button,
   Flex,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Heading,
   Input,
   Stack,
   Image,
 } from '@chakra-ui/react'
+
 import Base from '../../components/Base'
 
+import { useWallet } from '../../context'
+
+interface Inputs {
+  name: string
+  total_supply: number
+}
+
 export default function Minter() {
+  const { connected } = useWallet()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
+  }
+
   return (
     <Base title="Presalano: Minter">
       <Stack minH={'70vh'} direction={{ base: 'column', md: 'row' }}>
-        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Flex
+          p={8}
+          flex={1}
+          align={'center'}
+          justify={'center'}
+          as="form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <Stack spacing={4} w={'full'} maxW={'md'}>
             <Heading fontSize={'2xl'}>Mint tokens with presalano</Heading>
-            <FormControl id="token-name-input">
+            <FormControl
+              id="token-name-input"
+              isInvalid={errors.name ? true : false}
+            >
               <FormLabel>Token Name</FormLabel>
-              <Input type="text" rounded={'full'} />
+
+              <Input
+                type="text"
+                rounded={'full'}
+                isDisabled={!connected || isSubmitting}
+                {...register('name', { required: 'Name is required' })}
+              />
+
+              <FormErrorMessage>
+                {errors.name && errors.name.message}
+              </FormErrorMessage>
             </FormControl>
 
-            <FormControl id="token-supply-input">
+            <FormControl
+              id="token-supply-input"
+              isInvalid={errors.total_supply ? true : false}
+            >
               <FormLabel>Total Supply</FormLabel>
-              <Input type="number" rounded={'full'} />
+              <Input
+                type="number"
+                rounded={'full'}
+                isDisabled={!connected || isSubmitting}
+                {...register('total_supply', {
+                  required: 'Total supply is required',
+                })}
+              />
+
+              <FormErrorMessage>
+                {errors.total_supply && errors.total_supply.message}
+              </FormErrorMessage>
             </FormControl>
 
             <Stack spacing={6} pt={3}>
@@ -36,6 +93,9 @@ export default function Minter() {
                 _hover={{ bg: 'messenger.600' }}
                 variant={'solid'}
                 size="lg"
+                type="submit"
+                isLoading={isSubmitting}
+                isDisabled={!connected}
               >
                 Mint
               </Button>
