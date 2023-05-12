25363c10bc405cd89781653e0dc7b9574705d39f
prajnastra <coder.abhijit90@gmail.com>
2023-05-12 07:52:05 +0530

    add: theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Heading } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <Heading>Presalano</Heading>
+      </main>
     </>
   )
 }
