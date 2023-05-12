3494a167ca59e72a7110a8d43b2029b2cf136b5d
prajnastra <coder.abhijit90@gmail.com>
2023-05-12 11:23:44 +0530

    update: styel of statscard

--- components/Stats/StatsCard.tsx
+++ components/Stats/StatsCard.tsx
@@ -21,9 +21,8 @@ export default function StatsCard(props: StatsCardProps) {
     <Stat
       px={{ base: 2, md: 4 }}
       py={'5'}
-      shadow={'xl'}
       border={'1px solid'}
-      borderColor={useColorModeValue('gray.800', 'gray.500')}
+      borderColor={useColorModeValue('gray.300', 'gray.500')}
       rounded={'lg'}
     >
       <Flex justifyContent={'space-between'}>
