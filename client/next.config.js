9380734d27c3737f8fdf0cf9710b94d4da02a7b1
prajnastra <coder.abhijit90@gmail.com>
2023-05-13 20:39:07 +0530

    add: api in env & swr

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     MAINNET_BLOCKFOREST_KEY: process.env.MAINNET_BLOCKFOREST_KEY,
     PREPROD_BLOCKFOREST_KEY: process.env.PREPROD_BLOCKFOREST_KEY,
     PREVIEW_BLOCKFOREST_KEY: process.env.PREVIEW_BLOCKFOREST_KEY,
+    API: process.env.API,
   },
   webpack: function (config, options) {
     config.experiments = {
